f074bc14e1cdeec7fb0a33fa2955f2d0e5116e6a
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-07-29 06:40:49 +0700

    Fix typo in admin email

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     {
         \App\Models\User::create([
             'name' => 'Admin QuisArena',
-            'email' => env('ADMIN_EMAIL', 'projek.msyafiq19@gmailcom'),
+            'email' => env('ADMIN_EMAIL', 'projek.msyafiq19@gmail.com'),
             'password' => bcrypt(env('ADMIN_PASSWORD', 'rahasia123')),
             'role' => 'admin',
         ]);
